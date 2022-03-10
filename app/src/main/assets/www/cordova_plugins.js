2f52304546ce0c9d3076861586fac8df188f8cd0
Muhammad Saad Faisal <saad.faisal@systemsltd.com>
2022-03-10 17:09:44 +0500

    Initial commit

---
+++ app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "com.unikrew.faceoff.ABLPlugin",
+      "file": "plugins/com.unikrew.faceoff/www/ABLPlugin.js",
+      "pluginId": "com.unikrew.faceoff",
+      "clobbers": [
+        "cordova.plugins.ABLPlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.5",
+    "com.unikrew.faceoff": "0.1"
+  };
+});
---
+++ app/src/main/java/com/unikrew/faceoff/ABLPlugin/model/CnicPostParams.java
@@ -0,0 +1,26 @@
+package com.unikrew.faceoff.ABLPlugin.model;
+
+import com.unikrew.faceoff.ABLPlugin.model.CnicPostData;
+
+import java.io.Serializable;
+
+public class CnicPostParams implements Serializable {
+
+	private CnicPostData data = new CnicPostData();
+
+	public void setData(CnicPostData data){
+		this.data = data;
+	}
+
+	public CnicPostData getData(){
+		return data;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"CnicPostParams{" +
+			"data = '" + data + '\'' + 
+			"}";
+		}
+}
